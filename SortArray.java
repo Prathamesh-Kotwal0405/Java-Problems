c4243f9a32687454494babe2ccaa1c8207246ee5
Prathamesh-Kotwal0405 <prathameshk0405@gmail.com>
2025-08-04 14:39:15 +0530

    Create SortArray.java

---
+++ SortArray.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class SortArray {
+    public static void main(String[] args) {
+        // Sorting of array.
+        int[] rray = {1,5,4,6,8};
+        Arrays.sort(rray);
+        System.out.println(Arrays.toString(rray));
+    }
+}
